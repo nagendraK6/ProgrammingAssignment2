ab01434421cf2ac309eb1ed7ea9b68f95414280f
nagendraK6 <nagendra.msft@hotmail.com>
2015-05-24 17:45:46 +0100

    Soln of Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## @author Nagendra Kumar
+## @Coursera Assignment work
+
+## PROBLEM STATEMENT
+## CACHE THE CONTENT OF INVERSE OF MATRIX
 
 ## Write a short comment describing this function
+## The fllowing function prepares and stores the result of
+## cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_m <- NULL
+  set <- function(y) {
+     x <<- y
+     inverse_m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inverse_m <<- inverse
+  getinverse <- function() inverse_m
+  list(set = set, get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## The below function check whether inverse exists
+## If inverse exists it returns the result otherwise
+## it calculates the inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+
+  inverse_m <- x$getinverse()
+  if(!is.null(inverse_m)) {
+    message("getting cached data")
+    return(inverse_m)
+  }
+  data <- x$get()
+  inverse_m <- solve(data, ...)
+  x$setinverse(inverse_m)
+  inverse_m
 }
